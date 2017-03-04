1720bb59a8f78ffea2058b23ee11e21ccab8bb2a
obroybass <rbass@outbrain.com>
2017-03-05 00:07:04 +0200

    Split app to views, using ngRoute

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,20 @@
-var walleApp = angular.module('walleApp', ['ngTagsInput', 'ngNumeraljs', '720kb.tooltips']);
+var walleApp = angular.module('walleApp', ['ngRoute', 'ngTagsInput', 'ngNumeraljs', '720kb.tooltips']);
+
+walleApp.controller('MainController', ($scope, $route, $routeParams, $location) => {
+  $scope.$route = $route;
+  $scope.$location = $location;
+  $scope.$routeParams = $routeParams;
+});
+
+
+walleApp.config(function ($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('');
+  $routeProvider
+    .when("/main", {
+      templateUrl: "templates/main.html"
+    })
+    .when("/route", {
+      templateUrl: "templates/route.html"
+    })
+    .otherwise("/main");
+});
