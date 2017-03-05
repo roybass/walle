d7a47cc311ad21e2194eeb7cfa7d56091afe133c
obroybass <rbass@outbrain.com>
2017-03-05 11:09:21 +0200

    Supporting query parameters for constraints

--- client/js/constraints.js
+++ client/js/constraints.js
@@ -1,19 +1,22 @@
 walleApp.component('constraints', {
   templateUrl: 'templates/constraints.html',
-  controller: function ConstraintsController($scope, $http) {
+  controller: function ConstraintsController($scope, $http, $route) {
+
+    const params = $route.current.params;
     $scope.constraints = {
-      maxCash: 30000000, // Max available cash for trading
-      maxJumps: 10, // Max jumps
-      maxCapacity: 5100, // Cubic meters available for hauling
-      minProfit: 100000, // Minimum profit per trade (units * price diff)
-      fromSystemRadius: 0, // Radius (in jumps) from the 'fromSystems' array.
-      minSecurity: 0 // Minimum security status of from/to system.
+      maxCash: params['maxCash'] || 30000000, // Max available cash for trading
+      maxJumps: params['maxJumps'] || 10, // Max jumps
+      maxCapacity: params['maxCapacity'] || 5100, // Cubic meters available for hauling
+      minProfit: params['minProfit'] || 100000, // Minimum profit per trade (units * price diff)
+      fromSystemRadius: params['fromSystemRadius'] || 0, // Radius (in jumps) from the 'fromSystems' array.
+      minSecurity: params['minSecurity'] || 0 // Minimum security status of from/to system.
     };
 
     $scope.refresh = function () {
       console.log("Emitting refresh");
       $scope.constraints.regions = $scope.regions.map((item) => item.text).join(',');
       $scope.constraints.fromSystems = $scope.systems.map((item) => item.text).join(',');
+      //  $route.updateParams($scope.constraints);
       $scope.$parent.$emit('refresh', $scope.constraints);
     };
 
@@ -37,14 +40,17 @@ walleApp.component('constraints', {
       });
     };
 
-    $scope.regions = [
-      { text: 'Metropolis' },
-      { text: 'Heimatar' },
-      { text: 'Derelik' },
-      { text: 'Molden Heath' }
-    ];
-    $scope.systems = [
-      { text: 'Rens' }
-    ];
+    $scope.regions = (params['regions'] || 'Metropolis, Heimatar, Derelik, Model Heath').split(',').map((item) => {
+      return {
+        text: item
+      }
+    });
+
+
+    $scope.systems = (params['systems'] || 'Rens').split(',').map((item) => {
+      return {
+        text: item
+      }
+    });
   }
 });
