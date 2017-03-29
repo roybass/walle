5bd0b6af6dd131d0931776464024b70ede97ef5b
obroybass <rbass@outbrain.com>
2017-03-29 23:50:07 +0300

    Adding XML API client for system stats (kills)

---
+++ server/crest/xml.js
@@ -0,0 +1,56 @@
+const rp = require('request-promise');
+const fileStore = require('./file_store');
+const apiBaseUrl = 'https://api.eveonline.com/';
+const consts = require('./../const');
+var xmlParser = require('xml-parser');
+const log = require('../../logger');
+
+
+class XmlClient {
+
+  getSystemStats() {
+    const url = "map/kills.xml.aspx";
+    return this.getData(url, 10 * consts.MINUTE, true).then((result => {
+      if (!result) {
+        return null;
+      }
+      const rows = result.root.children[1].children[0];
+      const systemStats = {};
+      for (let row of rows.children) {
+        systemStats[row.attributes.solarSystemID] = row.attributes;
+      }
+      return systemStats;
+    }));
+  }
+
+
+  getData(relativeUrl, maxAge, useCache = true) {
+    return fileStore.get(relativeUrl, maxAge, useCache).then((data) => {
+      if (data !== undefined && data !== null) {
+        // log.debug('Found cached data for %s', relativeUrl);
+        return xmlParser(data);
+      }
+      const url = apiBaseUrl + relativeUrl;
+      log.debug('No cache found. Request data from %s', url);
+      return rp(url).then((response) => {
+        if (!response) {
+          log.warn('Error getting data from %s', url);
+        }
+        const data = response ? response : '{}';
+        return fileStore.set(relativeUrl, response).then(() => {
+            return xmlParser(response);
+          }
+        );
+      }).catch((err) => {
+        return fileStore.set(relativeUrl, '{}').then(() => {
+          return {};
+        });
+      });
+    });
+  }
+}
+
+new XmlClient().getSystemStats().then((stats) => {
+  console.log(stats);
+});
+module.exports = new XmlClient();
